f38928330db60f6a20815bb79d8c82a216ecd4cb
manh.pham <manh.pham@savvycomsoftware.com>
2024-07-04 23:46:21 +0700

    fix

--- src/services/worker.service.ts
+++ src/services/worker.service.ts
@@ -14,14 +14,15 @@ export class WorkerService {
   @Cron(CronExpression.EVERY_HOUR)
   async handleCron() {
     const listCamera = await this.camConfigRepository.findAll({
-      // where: {  },
+      where: {
+        active: true,
+      },
       relations: {
         cam: true,
         provider: true,
       },
     });
 
-    // const cameraUrl = 'rtsp://rtsp-test-server.viomic.com:554/stream'; // Replace with your RTSP URL
     for (const camConfig of listCamera) {
       if (!camConfig.provider) {
         throw new Error('Provider not found');
