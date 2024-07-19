eb35241f9e58c9e6dcc98c34dd49711d5e52a755
manh.pd <manh.pd@cmctelecom.vn>
2024-07-19 16:17:01 +0700

    Fix

--- src/controllers/cam.controller.ts
+++ src/controllers/cam.controller.ts
@@ -245,7 +245,7 @@ export class UserController {
   async CallbackTest(@Query('code') code: string) {
     console.log('code', code);
 
-    const CLIENT_ID = 'Iv23lilYDG3TcfcXZNCh';
+    const CLIENT_ID = 'Iv23li5gpJsQgYqCfUEX';
     const CLIENT_SECRET = env.get('github_client_secret');
     const baseServiceGithub = new BaseService(
       'https://github.com/login/oauth/access_token',
